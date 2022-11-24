626b33cca1b72119940317114a27783750103304
Mario Barceló <mariobarcelosospedra@gmail.com>
2022-11-24 18:12:01 +0100

    Improve dark/light mode

--- script.js
+++ script.js
@@ -1,71 +1,76 @@
-const body = document.body
-const getBodyTheme = localStorage.getItem('portfolio-theme')
-const getButtonTheme = localStorage.getItem('portfolio-button-theme')
-const buttonTheme = document.querySelector('.fa-moon')
-const buttonHamburger = document.querySelector('.fa-bars')
-const profilePhoto = document.querySelector('.profile-photo')
-const logo = document.querySelector('.logo')
+const body = document.body;
+const getBodyTheme = sessionStorage.getItem("portfolio-theme");
+const getButtonTheme = sessionStorage.getItem("portfolio-button-theme");
+const buttonTheme = document.querySelector(".fa-moon");
+const buttonHamburger = document.querySelector(".fa-bars");
+const profilePhoto = document.querySelector(".profile-photo");
+const logo = document.querySelector(".logo");
 
-const isDark = () => body.classList.contains('dark')
+const isDark = () => body.classList.contains("dark");
 
 const setTheme = (bodyClass, buttonClass) => {
-	body.classList.remove(localStorage.getItem('portfolio-theme'))
-	buttonTheme.classList.remove(localStorage.getItem('portfolio-button-theme'))
-	body.classList.add(bodyClass)
-	buttonTheme.classList.add(buttonClass)
-	localStorage.setItem('portfolio-theme', bodyClass)
-	localStorage.setItem('portfolio-button-theme', buttonClass)
+	body.classList.remove(sessionStorage.getItem("portfolio-theme"));
+	buttonTheme.classList.remove(
+		sessionStorage.getItem("portfolio-button-theme")
+	);
+	body.classList.add(bodyClass);
+	buttonTheme.classList.add(buttonClass);
+	sessionStorage.setItem("portfolio-theme", bodyClass);
+	sessionStorage.setItem("portfolio-button-theme", buttonClass);
 	changeProfilePhoto(bodyClass);
 	changeLogo(bodyClass);
-}
+};
 
-function changeProfilePhoto (bodyClass) {
-	if(bodyClass == "dark") {
-		profilePhoto.setAttribute("src","./img/mario-barcelo-profile-dark-mode.png");
+function changeProfilePhoto(bodyClass) {
+	if (bodyClass == "dark") {
+		profilePhoto.setAttribute(
+			"src",
+			"./img/mario-barcelo-profile-dark-mode.png"
+		);
 	} else {
-		profilePhoto.setAttribute("src","./img/mario-barcelo-profile.png");
+		profilePhoto.setAttribute("src", "./img/mario-barcelo-profile.png");
 	}
 }
 
-function changeLogo (bodyClass) {
-	if(bodyClass == "dark") {
-		logo.setAttribute("src","./img/mario-barcelo-logo-dark-mode.png");
+function changeLogo(bodyClass) {
+	if (bodyClass == "dark") {
+		logo.setAttribute("src", "./img/mario-barcelo-logo-dark-mode.png");
 	} else {
-		logo.setAttribute("src","./img/mario-barcelo-logo.png");
+		logo.setAttribute("src", "./img/mario-barcelo-logo.png");
 	}
 }
 
 const toggleTheme = () =>
-	isDark() ? setTheme('light', 'fa-moon') : setTheme('dark', 'fa-sun')
+	isDark() ? setTheme("light", "fa-moon") : setTheme("dark", "fa-sun");
 
 const displayList = () => {
-	const navUl = document.querySelector('.nav__list')
+	const navUl = document.querySelector(".nav__list");
 
-	if (buttonHamburger.classList.contains('fa-bars')) {
-		buttonHamburger.classList.remove('fa-bars')
-		buttonHamburger.classList.add('fa-times')
-		navUl.classList.add('display-nav-list')
+	if (buttonHamburger.classList.contains("fa-bars")) {
+		buttonHamburger.classList.remove("fa-bars");
+		buttonHamburger.classList.add("fa-times");
+		navUl.classList.add("display-nav-list");
 	} else {
-		buttonHamburger.classList.remove('fa-times')
-		buttonHamburger.classList.add('fa-bars')
-		navUl.classList.remove('display-nav-list')
+		buttonHamburger.classList.remove("fa-times");
+		buttonHamburger.classList.add("fa-bars");
+		navUl.classList.remove("display-nav-list");
 	}
-}
+};
 
-buttonTheme.addEventListener('click', toggleTheme)
-buttonHamburger.addEventListener('click', displayList)
+buttonTheme.addEventListener("click", toggleTheme);
+buttonHamburger.addEventListener("click", displayList);
 
-const setBodyTheme = () => body.classList.add(getBodyTheme)
-const setButtonTheme = () => buttonTheme.classList.add(getButtonTheme)
+const setBodyTheme = () => body.classList.add(getBodyTheme);
+const setButtonTheme = () => buttonTheme.classList.add(getButtonTheme);
 
-setBodyTheme()
-setButtonTheme()
+setBodyTheme();
+setButtonTheme();
 
 // Heart Animation
-const heart = document.querySelector('.heart');
+const heart = document.querySelector(".heart");
 
-heart.addEventListener('click', heartToggle);
+heart.addEventListener("click", heartToggle);
 
-function heartToggle () {
-	heart.classList.toggle('heart-animation');
+function heartToggle() {
+	heart.classList.toggle("heart-animation");
 }
